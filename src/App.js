f00caa8c0e5732e9761fdc33bedb6454607d9033
karan <kc80465@gmail.com>
2023-05-03 21:17:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import "./App.css";
+import Hero from "./components/Hero";
+import Footer from "./components/footer/Footer";
+import Join from "./components/joinUS/Join";
+import Plans from "./components/plans/Plans";
+import Program from "./components/program/Program";
+import Reson from "./components/reason/Reson";
+import Testimonial from "./components/tesimonial/Testimonial";
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <Hero />
+      <Program />
+      <Reson />
+      <Plans />
+      <Testimonial />
+      <Join />
+      <Footer />
     </div>
   );
 }
